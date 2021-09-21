e1de0de80450b69f984beae8c9f206b7b8d2281f
Amy Posten <amyposten85@gmail.com>
2021-09-21 17:28:07 -0400

    commit tree 33761d67a5fdfbaab555de64f8c9ad92b2233a5b

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+// Declare variable returnFirstTwo drivers with const and assign an anonymous function
+const returnFirstTwoDrivers = function (firstDrivers) { //function should accept an array of drivers as an arguement
+    return firstDrivers.slice(0, 2)
+} 
+// and return the first two drivers in an array
+
+// Declare a variable returnLastTwoDrivers with const that is assigned to anonymous function
+const returnLastTwoDrivers = function (lastDrivers) { //function should accept an array of drivers and return the last two drivers in the array
+    return lastDrivers.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+// Declare higher-order function, createFareMultiplier that takes in one argument, an integer
+
+function createFareMultiplier (integer) {
+    return function (fare) {
+        return fare * integer
+    }
+} 
+// return a function that takes in the fare 
+
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers (arrayOfDrivers, driverFunction) {
+    return driverFunction(arrayOfDrivers);
+}
